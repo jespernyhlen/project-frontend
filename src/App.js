db7dbbfb3c79b3b732c81a9e2c06ba65b3fea00f
Jesper Nyhlen <jeppe_nyhlen@hotmail.com>
2019-10-26 00:03:57 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-
 import NavBar from './components/NavBar';
 import Login from './components/Login';
 import Profile from './components/Profile';
